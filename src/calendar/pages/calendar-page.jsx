dcb4c1c0ef537b241c84d74bdb757122da5d15bc
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-02 17:30:20 -0300

    wip: calendar page

--- src/calendar/pages/calendar-page.jsx
+++ src/calendar/pages/calendar-page.jsx
@@ -1,9 +1,46 @@
+import { addHours, format, getDay, parse, startOfWeek } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Navbar } from '../components/nav-bar';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: 'titulo',
+    notes: 'notas',
+    start: new Date(),
+    end: addHours(new Date(), 1),
+    bgColor: '#fafafa',
+    user: {
+      _id: 'asdasd',
+      name: 'nombre',
+    },
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)', padding: '1rem' }}
+      />
     </>
   );
 };
