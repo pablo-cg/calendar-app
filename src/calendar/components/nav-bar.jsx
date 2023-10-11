38ddc28c59ce707dd82d9e9ad72c0d9a0aa072bc
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-11 15:10:16 -0300

    refactor: agregado logout y nombre de usuario

--- src/calendar/components/nav-bar.jsx
+++ src/calendar/components/nav-bar.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import { useAuthStore } from '../../hooks/use-auth-store';
 
 export const Navbar = () => {
+  const { user, startLogout } = useAuthStore();
+
   return (
     <div className="navbar navbar-dark bg-dark px-4 mb-4">
       <span className="navbar-brand">
         <i className="fas fa-calendar-alt"></i>
-        &nbsp; Pablo
+        &nbsp; {user?.name}
       </span>
-      <button className="btn btn-outline-danger">
+      <button className="btn btn-outline-danger" onClick={startLogout}>
         <span>
           <i className="fas fa-sign-out-alt"></i>
           &nbsp; Salir
